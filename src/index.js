42d88599346e31113778227295bf94d76e25646c
MadNoodle <mjanneau@gmail.com>
2017-09-26 18:54:49 +0200

    final commit

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+//JS PERSO
+import {sampleText} from './sampleText';
+import marked from 'marked';
+
+//CSS
+import './style/css/bootstrap.min.css';
+import './index.css';
+
+class App extends React.Component{
+    state = {
+        text:sampleText
+    };
+
+    componentWillMount(){
+        const text = localStorage.getItem('text');
+        if (text) {
+            this.setState({ text});
+        }
+    }
+    componentWillUpdate(nextProps, nextState) {
+        localStorage.setItem('text', nextState.text);
+    };
+
+
+    editText = (event) =>{
+        const text = event.target.value;
+        this.setState({
+            text
+        });
+    };
+
+    renderText(text){
+        const renderText = marked(text, {sanitize :true});
+        return {__html: renderText};
+    }
+    
+    
+    render() {
+        return(
+             <div className="container">
+                 <div className="row">
+                     <div className="col-sm-6">
+                        <textarea rows="35" className="form-control" value={this.state.text} onChange={(e) => this.editText(e)}>
+
+                        </textarea>
+                     </div>
+                     <div className="col-sm-6">
+                        <div dangerouslySetInnerHTML={this.renderText(this.state.text)}/>
+                        
+                     </div>
+                </div>
+             </div>
+        );
+    };
+}
+
+render (<App />, document.getElementById('root'));
